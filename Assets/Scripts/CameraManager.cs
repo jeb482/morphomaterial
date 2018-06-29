c6edab725e0dd842743670e237a71f12d5f3f1f4
Jimmy B <jeb482@cornell.edu>
2018-06-29 03:24:44 -0400

    Added zoom to the viewport. Removed dead code from CameraManager

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -88,78 +88,13 @@ public class CameraManager : MonoBehaviour {
                 break;
             case CameraConfiguration.ViewportCam:
                 viewportCam.orbitAndZoom();
-                //GetOrbitInput(Viewport.GetComponent<Camera>(), Viewport.transform);
-                //Draw(Viewport.GetComponent<Camera>(), Viewport.transform, zoomInput);
                 break;
             default:
                 return;
         }
         
-        
-
+        // Guard against focus being changed externally
         LastFocus = Focus;
-    }
-
-    void GetOrbitInput(Camera currentCam, Transform currentCamXform)
-    {
-        if (Input.GetMouseButtonDown(0) && Input.GetKey(KeyCode.LeftAlt))
-        {
-            originalMousePos = Input.mousePosition;
-            
-            worldX = (currentCam.ScreenToWorldPoint(new Vector3(100, 0, 1)) - currentCam.ScreenToWorldPoint(new Vector3(0, 0, 1))).normalized;
-            worldY = (currentCam.ScreenToWorldPoint(new Vector3(0, 100, 1)) - currentCam.ScreenToWorldPoint(new Vector3(0, 0, 1))).normalized;
-            
-            isOribiting = true;
-            originalPosition = currentCamXform.position;
-            originalRotation = currentCamXform.rotation;
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            isOribiting = false;
-        }
-    }
-     
-    /// <summary>
-    /// Sets the camera transformation given it's orinetation and distance with respect to the Focus
-    /// </summary>
-    void Draw(Camera currentCam, Transform currentCamXform, float zoomInput)
-    {
-
-        CameraDistance = System.Math.Min(MaximumCameraDistance, System.Math.Max(MinimumCameraDistance, CameraDistance + zoomInput));
-        // Update transformation based on orbit
-        if (isOribiting && Focus != null)
-        {
-            Vector3 delta = Input.mousePosition - originalMousePos;
-            currentCamXform.SetPositionAndRotation(originalPosition, originalRotation);
-
-            // Don't let user rotate over y axis.{
-            var oldViewDir = currentCamXform.InverseTransformVector(new Vector3(0, 0, 1)).normalized;
-            currentCamXform.RotateAround(Focus.transform.position, worldX, RotationSensitivity * -delta.y);
-            var newViewDir = currentCamXform.InverseTransformVector(new Vector3(0, 0, 1)).normalized;
-            if ((lockDeltaY && System.Math.Abs(delta.y) > System.Math.Abs(lastDeltaY)) || (System.Math.Sign(newViewDir.x) != System.Math.Sign(oldViewDir.x) || System.Math.Sign(newViewDir.z) != System.Math.Sign(oldViewDir.z) && (oldViewDir.x != 0 || oldViewDir.z != 0)))
-            {
-                currentCamXform.RotateAround(Focus.transform.position, worldX, RotationSensitivity * delta.y - lastDeltaY);
-                lockDeltaY = true;
-            }
-            else
-            {
-                lockDeltaY = false;
-                lastDeltaY = delta.y;
-                currentCamXform.RotateAround(Focus.transform.position, worldY, RotationSensitivity * delta.x);
-            }
-
-            // Rotate about Y
-            currentCamXform.LookAt(Focus);
-            if (Focus != null)
-                currentCamXform.localPosition *= (CameraDistance / currentCamXform.localPosition.magnitude);
-           
-        }
-
-//        // Update distance
-//        if (Focus != null)
-//        {
-//            currentCamXform.localPosition *= (CameraDistance / currentCamXform.localPosition.magnitude);
-//        }
     }
 
     void SetConfig(CameraConfiguration config)
@@ -222,31 +157,4 @@ public class CameraManager : MonoBehaviour {
                 return new Vector3(0, 0, 0);
         }
     }
-
-    public void RecenterFishTank()
-    {
-        Debug.Log("Re-centering FishTank");
-
-        // The vector from the screen center to the user's eye .
-        Vector3 oldScreenCenter = (GameController.Instance.lowerLeftScreenCorner + GameController.Instance.upperRightScreenCorner) / 2;
-        Vector3 centerDiff = EyeLocation.TransformPoint(GameController.Instance.fishtankEyeOffset) - oldScreenCenter;
-
-        // The vector of the user's nose points in.
-        Vector3 headForward = EyeLocation.TransformPoint(GameController.Instance.fishtankEyeOffset) - FishtankCamObj.transform.position;
-        headForward.y = 0;
-        headForward.Normalize();
-
-        // Subtract out component of difference parallel to user's gaze.
-        centerDiff -= Vector3.Dot(centerDiff, headForward) * headForward;
-
-
-        GameController.Instance.lowerLeftScreenCorner += centerDiff;
-        GameController.Instance.upperLeftScreenCorner += centerDiff;
-        GameController.Instance.upperRightScreenCorner += centerDiff;
-
-        GameController.Instance.updateRealWorldToScreen();
-
-        //ConfigureFishtank();
-    }
-
 }
--- Assets/Scripts/ViewportCamera.cs
+++ Assets/Scripts/ViewportCamera.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class ViewportCamera : MonoBehaviour {
     public Transform Focus;
     public float RotationSensitivity = 1;
+    public float distance = 1;
+    public float ZoomSensitivity = 1;
+    Vector2 validDistanceRange = new Vector2(.5f, 5);
 
     private Camera cam;
     private bool isOrbiting = false;
@@ -33,7 +36,7 @@ public class ViewportCamera : MonoBehaviour {
             return;
         }
         
-        // Handle input and revolution.
+        // Handle input for revolution.
         if (isOrbiting && Focus != null)
         {
             Vector3 screenX = (cam.ScreenToWorldPoint(new Vector3(100, 0, 1)) - cam.ScreenToWorldPoint(new Vector3(0, 0, 1))).normalized;
@@ -55,6 +58,13 @@ public class ViewportCamera : MonoBehaviour {
             lastPosition = transform.position;
             lastRotation = transform.rotation;
         }
+
+        float zoomInput = Input.GetAxis("Mouse ScrollWheel");
+        if (zoomInput == 0f)
+            return;
+        distance = System.Math.Min(validDistanceRange.y, System.Math.Max(validDistanceRange.x, distance + ZoomSensitivity * -zoomInput));
+        Vector3 displacement = transform.position - Focus.position;
+        transform.position = Focus.position + (displacement.normalized) * distance;
 
     }
     // Update is called once per frame
